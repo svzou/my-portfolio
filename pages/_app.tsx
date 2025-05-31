03040dafc733366f7d7646cd4ae398924df1dcac
Sophia Zou <111460074+svzou@users.noreply.github.com>
2025-05-31 13:39:55 -0400

    google analytics tracking

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,7 @@ import Experience from "@/components/experience";
 import { Urbanist } from "next/font/google"
 import Classes from "@/components/classes";
 import Footer from "@/components/footer";
+import Script from "next/script";
 
 const urbanist = Urbanist({
   subsets: ["latin"],
@@ -17,6 +18,27 @@ const urbanist = Urbanist({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
+    <>
+    <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-VRVLWR5JPR`} // 👈 Replace with your ID
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-VRVLWR5JPR', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+    
+
     <div className={urbanist.variable}>
       <Nav />
       <Hero />
@@ -26,5 +48,6 @@ export default function App({ Component, pageProps }: AppProps) {
       <Footer />
 
     </div>
+    </>
   )
 }
